5ad863aa34d7f0570f497367a5b40ff67eaa5aab
NatashaTamashiro <nat.tamashiro@hotmail.com>
2020-06-14 22:47:21 -0300

    iniciando quasar

---
+++ src/pages/Help.vue
@@ -0,0 +1,13 @@
+<template>
+  <q-page class="q-pa-lg">
+   <h5 class="q-mt-none">Help</h5>
+   <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Veritatis nemo corrupti numquam iusto repellendus eligendi sunt velit. Modi, quisquam magni. Doloremque veniam, asperiores saepe tenetur reprehenderit voluptates similique quis doloribus.</p>
+   <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Veritatis nemo corrupti numquam iusto repellendus eligendi sunt velit. Modi, quisquam magni. Doloremque veniam, asperiores saepe tenetur reprehenderit voluptates similique quis doloribus.</p>
+
+  </q-page>
+</template>
+
+<script>
+export default {
+}
+</script>
